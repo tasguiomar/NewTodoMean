67592a86ef3b7877fe3dd949527bd678b4c1db8c
Unknown <tiagoalexandresanto@gmail.com>
2018-11-08 14:57:45 +0000

    mongo reference between models

--- models/db.js
+++ models/db.js
@@ -1,10 +1,11 @@
 var mongoose = require( 'mongoose' );
+var Schema = mongoose.Schema;
 
 
 var bookmarkSchema = new mongoose.Schema({
   link: {type: String},
   description: {type: String},
   created_at:{type:Date,default:Date.now},
-  created_by:String
+  created_by: {type: Schema.Types.String, ref: 'users'}
 });
 mongoose.model('Bookmark', bookmarkSchema,'bookmarks' );
